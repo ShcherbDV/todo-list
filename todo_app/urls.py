57555f424054d642e81f7da6b1a269a30dac212e
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-30 20:53:40 +0200

    update urls.py->fix paths to create & update task views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -14,8 +14,8 @@ from todo_app.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
