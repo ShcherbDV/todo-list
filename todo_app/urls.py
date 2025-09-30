641b3ea1f317cd03b419eb9176a14bb9331eff3e
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-30 19:40:04 +0200

    update todo_app/urls.py->add path to tags list

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo_app.views import index
+from todo_app.views import index, TagListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo_app"
