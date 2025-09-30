521aff55cadc835691e86da74c5d0d6371a2a2d7
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-30 22:33:25 +0200

    update views.py->add toggle_task function

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -8,7 +8,7 @@ from todo_app.views import (
     TagDeleteView,
     TaskCreateView,
     TaskUpdateView,
-    TaskDeleteView,
+    TaskDeleteView, toggle_task,
 )
 
 urlpatterns = [
@@ -16,6 +16,7 @@ urlpatterns = [
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/toggle/", toggle_task, name="task-toggle"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
