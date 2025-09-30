dcedc1eced6c2b9d41267ad8c48195dd353a6605
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-30 19:51:13 +0200

    update views.py->add index function & TagListView

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo_app.models import Tag, Task
+
+
+def index(request):
+
+    queryset = Task.objects.all().order_by("is_done").order_by("datetime")
+
+
+    context = {
+        "task_list": queryset,
+    }
+
+    return render(request, "todo_app/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
