ee22f97ae4acfc608a409cb749edb5f80dc2353a
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-30 20:00:14 +0200

    update views.py->add TagCreate/Update/Delete views

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_app.models import Tag, Task
@@ -18,3 +19,20 @@ def index(request):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_app:tag-list")
